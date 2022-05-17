574946bec4ec97c15d8a987f8dd4de4997fcdc9b
Vova Nagornyj <vova-nagornyj@mail.ru>
2022-05-17 22:21:53 +0700

    set function to time check

--- callboard_back/src/sockets/index.js
+++ callboard_back/src/sockets/index.js
@@ -1,7 +1,20 @@
 import { Server } from "socket.io";
+import dotenv from "dotenv";
+import {auctionFinder} from "./auction.handler.js";
+
+dotenv.config();
 
 export default function InitSocketIO(server){
-    const io = new Server(server)
+    const io = new Server(server, {
+        cors:{
+            origin: `http://localhost:${process.env.PORT || 3070}`,
+            credentials: true
+        }
+    })
 
-
+    io.on('connection', async (socket)=>{
+        try{
+            socket.on('auction: finder', async ()=>{await auctionFinder(socket)})
+        }catch (err){}
+    })
 }
