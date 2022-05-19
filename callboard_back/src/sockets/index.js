181a75684f0f7f3d6646ccd98683193b03805bb0
Vova Nagornyj <vova-nagornyj@mail.ru>
2022-05-19 16:49:57 +0700

    add method path to chat functional

--- callboard_back/src/sockets/index.js
+++ callboard_back/src/sockets/index.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import dotenv from "dotenv";
 import {auctionFinder, newPrice} from "./auction.handler.js";
+import {messageHandler} from "./message.handler.js";
 
 dotenv.config();
 
@@ -16,6 +17,9 @@ export default function InitSocketIO(server){
         try{
             socket.on('auction: finder', async ()=>{await auctionFinder(socket)})
             socket.on('auction: price_updater', async (data)=>{await newPrice(socket, data)})
+            socket.on('message: handler',async (data)=>{await messageHandler(socket, data)})
+            socket.on('chat: join', async (data)=>{socket.join(data.chat_id)})
+            socket.on('chat: leave', async (data)=>{socket.leave(data.chat_id)})
         }catch (err){
             socket.emit('socket: err', {
                     status: 'Error',
