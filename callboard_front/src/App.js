2aa80a5d57b53ed112e27fd1f121ebbea17f2458
Vova Nagornyj <vova-nagornyj@mail.ru>
2022-06-07 22:35:45 +0700

    minor refactoring

--- callboard_front/src/App.js
+++ callboard_front/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
+import 'rsuite/dist/rsuite.min.css';
+
 import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
     return(
       <BrowserRouter>
           <Routes>
-            <Route path='' element={<MainPage/>}/>
+              <Route path='' element={<MainPage/>}/>
           </Routes>
       </BrowserRouter>
     )
