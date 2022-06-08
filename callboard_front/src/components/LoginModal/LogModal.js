162e2e9a51c4a3bdc737bf2527c20688652ca5be
Vova Nagornyj <vova-nagornyj@mail.ru>
2022-06-08 22:30:05 +0700

    complate log modal (no api)

---
+++ callboard_front/src/components/LoginModal/LogModal.js
@@ -0,0 +1,114 @@
+import React, {useState} from "react";
+import {
+    Modal,
+    Button,
+    Form,
+    Schema,
+    InputGroup,
+} from 'rsuite';
+
+import RegModal from "../../components/RegModal/RegModal";
+
+import CheckIcon from '@rsuite/icons/Check';
+import CloseIcon from '@rsuite/icons/Close';
+
+import { ReactComponent as Logo } from '../../misc/img/logo.svg';
+
+import './LogModalStyle.css'
+
+const {StringType} = Schema.Types
+
+const model = Schema.Model({
+    email: StringType()
+        .isEmail('Пожалуйста, введите почту правльно!')
+        .isRequired('Это необходимое поле!'),
+    password: StringType().isRequired('Это необходимое поле!')
+        .minLength(8, 'Необходимо минимум 8 символов')
+})
+
+
+function TextField(props) {
+    const { name, label, accepter, ...rest } = props;
+    return (
+        <Form.Group controlId={`${name}-3`}>
+            <Form.Control placeholder={label} errorPlacement={'bottomEnd'} name={name} accepter={accepter} {...rest} />
+        </Form.Group>
+    );
+}
+
+function CustomTextField(props) {
+    const { name, label, accepter, ...rest} = props;
+    const [visible, setVisible] = React.useState(false);
+
+
+    const handleChange = () => {
+        setVisible(!visible);
+    };
+
+    return (
+        <Form.Group controlId={`${name}-3`} >
+            <InputGroup style={{width: "100%"}}>
+                <Form.Control placeholder={label} errorPlacement={'bottomEnd'} name={name} type={visible ? 'text' : 'password'} accepter={accepter} {...rest} />
+                <InputGroup.Addon onClick={handleChange}>
+                    {visible ? <CheckIcon /> : <CloseIcon />}
+                </InputGroup.Addon>
+            </InputGroup>
+        </Form.Group>
+    );
+}
+
+export default function LogModal(props){
+    const [regModal, openRegModal] = useState(false)
+    const [formError, setFormError] = useState({})
+    const [formValue, setFormValue] = useState({
+        email: '',
+        password: '',
+    });
+
+    const logInAvailable = Object.keys(formError).length !== 0
+        || formValue.email.length === 0
+        || formValue.password.length === 0
+
+
+    //---------------------------------------------todo: api insert!
+    const loginHandler = () => {
+
+    }
+
+    return(
+        <div>
+            <RegModal
+                open={regModal}
+                onClose={()=>{openRegModal(false)}}
+            />
+            <Modal className={'modal-root'} open={props.open} onClose={props.onClose} overflow={false} backdrop={"static"}>
+                <Modal.Header className={'header-style'}>
+                    <Logo style={{width: "4em"}}/>
+                    <Modal.Title><b>Добро пожаловать!</b></Modal.Title>
+                    <Modal.Title><b>Вход</b></Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form model={model} fluid onChange={setFormValue} formValue={formValue} onCheck={setFormError}>
+                        <TextField name="email" label="Ваша электронная почта" fieldError={formError}/>
+                        <CustomTextField name="password" label="Пароль" fieldError={formError} autoComplete="off"/>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <div className={'header-style'}>
+                        <Button onClick={props.onClose} appearance="primary" disabled={logInAvailable}>
+                            Войти
+                        </Button>
+                        <Button onClick={props.onClose} appearance="subtle">
+                            Отмена
+                        </Button>
+                    </div>
+                    <hr/>
+                    <div className={'reg-link'}>
+                        <Modal.Title className={'header-style'}><b>Еще не зарегестрированы?</b></Modal.Title>
+                        <Button appearance="link" onClick={()=>{openRegModal(true)}}><b>Тогда присоединяйтесь!</b></Button>
+                    </div>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
+}
