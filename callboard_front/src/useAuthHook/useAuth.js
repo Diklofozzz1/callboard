067f5bc1edee1bc5fc8a5d2f34668a3f511ef964
Vova Nagornyj <vova-nagornyj@mail.ru>
2022-06-14 21:27:20 +0700

    fix auth logic

--- callboard_front/src/useAuthHook/useAuth.js
+++ callboard_front/src/useAuthHook/useAuth.js
@@ -1,55 +1,57 @@
-import React, { createContext, ReactNode, useContext, useEffect, useMemo, useState } from 'react'
+import React, {createContext, ReactNode, useContext, useEffect, useMemo, useState} from 'react'
 
-import { Login, ShowMe } from '../api/auth/auth.services'
+import {Login, ShowMe} from '../api/auth/auth.services'
 import {useLocation} from "react-router-dom";
+import authHeader from "../api/auth/auth.header";
 
 const AuthContext = createContext({})
 
-export function AuthProvider({children}){
+export function AuthProvider({children}) {
     const [user, setUser] = useState(undefined)
     const [error, setError] = useState(undefined)
     const location = useLocation()
 
-    useEffect(()=>{
-        if(error){
+    useEffect(() => {
+        if (error) {
             setError(undefined)
         }
     }, [location.pathname])
 
-    useEffect(()=>{
-        if(user===undefined){
+    useEffect(() => {
+        if (user === undefined) {
             me()
         }
-    },[])
+    }, [])
 
-    function me(){
-        ShowMe().then(updated=>{
-            setUser(updated.data.data)
-            console.log('USER updated')
-        }).catch(err=>{
+    function me() {
+        ShowMe().then(updated => {
+            setUser(updated)
+        }).catch(err => {
+            console.log(err)
             setError(err.response)
         })
     }
 
-    async function login(props){
-        try{
+    async function login(props) {
+        try {
             const result = await Login(props)
-            if (result.token!==undefined){
+            if (result.token !== undefined) {
                 localStorage.setItem('token', result.token)
+                me()
                 return true
             }
-        }catch(err){
+        } catch (err) {
             return err.response?.status
         }
     }
 
-    function logout(){
+    function logout() {
         localStorage.removeItem('token')
         setUser(undefined)
         setError(undefined)
     }
 
-    const memValue = useMemo(()=>({
+    const memValue = useMemo(() => ({
         user,
         error,
         login,
@@ -64,6 +66,6 @@ export function AuthProvider({children}){
     )
 }
 
-export default function useAuth(){
+export default function useAuth() {
     return useContext(AuthContext)
 }
